f77a20168fc7194279b3115945594ad4c7b8e39d
brassart <florent.brassart@epitech.eu>
2022-04-11 20:54:22 +0200

    async ipc

--- main.js
+++ main.js
@@ -1,14 +1,7 @@
 // Modules to control application life and create native browser window
-const electron = require("electron");
-
-const { app, BrowserWindow, ipcMain } = electron;
+const { app, BrowserWindow, ipcMain } = require("electron");;
 
 const path = require('path')
-
-// import { communicator } from "./communicator.js";
-
-const { communicator } = require('./communicator')
-// let form = document.querySelector("form")
 
 function createWindow () {
   // Create the browser window.
@@ -50,73 +43,9 @@ app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
-// receive message from index.html 
-// ipcMain.on('asynchronous-message', (event, arg) => {
-//     console.log( arg , process.cwd());
-    
-//     // send message to index.html
-//     event.sender.send('asynchronous-reply', 'hello' );
-// });
-/**************************************************************************************************** */
-
-// const { app, BrowserWindow, ipcMain } = require('electron')
-
-// let mainWindow
-
-// function createWindow () {
-//     const mainWindow = new BrowserWindow({
-//         width: 800,
-//         height: 600,
-//         webPreferences: {
-//             preload: path.join(__dirname, 'preload.js'),
-//             nodeIntegration: true,
-//             contextIsolation: false
-//         }
-//     })
-//     mainWindow.loadFile('index.html')
-
-//     // Open the DevTools.
-//     // mainWindow.webContents.openDevTools()
-
-//     mainWindow.on('closed', () => {
-//         mainWindow = null
-//     })
-// }
-
-// app.on('ready', createWindow)
-
-// app.on('window-all-closed', () => {
-//     if (process.platform !== 'darwin') {
-//         app.quit()
-//     }
-// })
-
-// app.on('activate', () => {
-//     if (mainWindow === null) {
-//         createWindow()
-//     }
-// })
-
-// Receive async message from renderer
-// See file renderer.js on line 3
-ipcMain.on('ping-good', event => {
-    // It's so good because below have a delay 5s to execute, and this don't lock rendereder :)
-    setTimeout(() => {
-        console.log('GOOD finshed!')
-        // Send reply to a renderer
-        event.sender.send('ping-good-reply', 'pong')
-    }, 5000)
-})
-
-// Receive sync message from renderer
-// See file renderer.js on line 18
-ipcMain.on('ping-bad', event => {
-    // It's so bad because below have a delay 5s to execute, meanwhile the renderer stay locked :(
-    setTimeout(() => {
-        console.log('BAD finshed!')
-        event.returnValue = 'pong'
-    }, 5000)
+ipcMain.on('asyncChannelToMain', (event, arg) => {
+    console.log(arg + ' from renderer')
+    if (arg === 'hello') {
+        event.sender.send('asyncChannelToRenderer', 'world')
+    }
 })
--- renderer.js
+++ renderer.js
@@ -5,17 +5,9 @@
 // selectively enable features needed in the rendering
 // process.
 
-document.querySelector('#submit').addEventListener('click', function() {
-  
-    let username = document.getElementById("username").value;
-
-    const {ipcRenderer} = require('electron')
-
-    // send username to main.js 
-    ipcRenderer.send('asynchronous-message', username )
-
-    // receive message from main.js
-    ipcRenderer.on('asynchronous-reply', (event, arg) => {
-        console.log(arg)
-    })
-});
+require('electron').ipcRenderer.on('asyncChannelToRenderer', (event, arg) => {
+    console.log(arg + ' from main')
+    if (arg === 'hello') {
+        event.sender.send('asyncChannelToMain', 'world')
+    }
+})
